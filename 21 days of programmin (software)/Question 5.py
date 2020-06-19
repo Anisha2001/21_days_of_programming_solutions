e2c065a26d962ee386b2c840e7e6d3b5658b9c75
Anisha2001 <58504594+Anisha2001@users.noreply.github.com>
2020-06-20 01:42:24 +0530

    Add files via upload

---
+++ 21 days of programmin (software)/Question 5.py
@@ -0,0 +1,12 @@
+#Convertion of binary to character
+
+b_num = list(input("Input a binary number: "))
+value = 0
+
+for i in range(len(b_num)):
+	digit = b_num.pop()
+	if digit == '1':
+		value = value + pow(2, i)
+print(chr(value))        
+
+#EASTER EGG: EUREKA
